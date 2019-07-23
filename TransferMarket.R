c98bbd52eb4cbcca8df241ad0a829b3cd57d381b
Gustavo Fabricio <gustavo.fabricio@tvglobo.com.br>
2019-07-23 18:24:52 -0300

    Inclusão de Partidas e Tabelas de pontos

--- TransferMarket.R
+++ TransferMarket.R
@@ -1,3 +1,5 @@
+#Bibliotecas
+
 require(rvest)
 require(selectr)
 require(xml2)
@@ -10,11 +12,11 @@ getwd()
 
 cod_england_TM <- read.csv('cod_england_TM.csv', sep = ';',stringsAsFactors = FALSE)
 
-PastSeasons <- seq(2016,2018,1)
+PastSeasons <- seq(2010,2018,1)
 
 #Gerando vetor de Clubes da Premiere League desde 2010 
 
-Clubs_full <- NULL
+TeamsFull <- NULL
 
 for (j in PastSeasons) {
   
@@ -23,22 +25,22 @@ url = paste("https://www.transfermarkt.pt/premier-league/startseite/wettbewerb/G
 
 scraped_page <- read_html(url)
 
-clubs <- scraped_page %>% html_nodes("#yw1 .no-border-links") %>% html_text() %>% as.character()
-clubs <- clubs[seq(1, length(clubs), by = 2)] %>%  
+Teams <- scraped_page %>% html_nodes("#yw1 .no-border-links") %>% html_text() %>% as.character()
+Teams <- Teams[seq(1, length(Teams), by = 2)] %>%  
          tolower() %>% 
          trimws("r", whitespace = "[\\h\\v]") 
 
-clubs <- gsub(' ',"-", clubs, fixed = TRUE)
+Teams <- gsub(' ',"-", Teams, fixed = TRUE)
 
-clubs <- data.frame(clubs)
+Teams <- data.frame(Teams)
 
-clubs$Season <- j 
+Teams$Season <- j 
 
-Clubs_full <- bind_rows(Clubs_full,clubs)
+TeamsFull <- bind_rows(TeamsFull,Teams)
 
 }
 
-Clubs_full <- left_join(Clubs_full ,cod_england_TM , by = c("clubs" = "Club"))
+TeamsFull <- left_join(TeamsFull ,cod_england_TM , by = c("Teams" = "Club"))
 
 
 ####################################
@@ -47,23 +49,24 @@ Clubs_full <- left_join(Clubs_full ,cod_england_TM , by = c("clubs" = "Club"))
 
 Players <- NULL
 
-for (j in seq(1,60,1)) {
+for (j in seq(1,nrow(TeamsFull),1)) {
 
     
-url = paste("https://www.transfermarkt.pt/", Clubs_full$clubs[j], "/kader/verein/", Clubs_full$Cod[j], "/plus/1/galerie/0?saison_id=", Clubs_full$Season[j], sep='')
+url = paste("https://www.transfermarkt.pt/", TeamsFull$Teams[j], "/kader/verein/", TeamsFull$Cod[j], "/plus/1/galerie/0?saison_id=", TeamsFull$Season[j], sep='')
   
 scraped_page <- read_html(url)
   
 PlayerNames  <- scraped_page %>% html_nodes(".spielprofil_tooltip") %>% html_text() %>% as.character()
 
+
   
-Position     <- scraped_page %>% html_nodes(".inline-table tr+ tr td")       %>% html_text() %>% as.character()
-Number       <- scraped_page %>% html_nodes(".rn_nummer")       %>% html_text() %>% as.character()
-Birth        <- scraped_page %>% html_nodes(".posrela+ .zentriert") %>% html_text() %>% as.character()
-Altura       <- scraped_page %>% html_nodes("td:nth-child(6)")   %>% html_text() %>% as.character()
-Pe_preferido <- scraped_page %>% html_nodes("td:nth-child(7)")   %>% html_text() %>% as.character()
-data_chegada <- scraped_page %>% html_nodes("td:nth-child(8)")   %>% html_text() %>% as.character()
-Valor        <- scraped_page %>% html_nodes(".rechts.hauptlink") %>% html_text() %>% as.character()
+Position <- scraped_page %>% html_nodes(".inline-table tr+ tr td")       %>% html_text() %>% as.character()
+Number   <- scraped_page %>% html_nodes(".rn_nummer")       %>% html_text() %>% as.character()
+Birth    <- scraped_page %>% html_nodes(".posrela+ .zentriert") %>% html_text() %>% as.character()
+Height   <- scraped_page %>% html_nodes("td:nth-child(6)")   %>% html_text() %>% as.character()
+Foot     <- scraped_page %>% html_nodes("td:nth-child(7)")   %>% html_text() %>% as.character()
+Joined  <- scraped_page %>% html_nodes("td:nth-child(8)")   %>% html_text() %>% as.character()
+Value   <- scraped_page %>% html_nodes(".rechts.hauptlink") %>% html_text() %>% as.character()
   
   
 df1 <- data.frame(PlayerNames)
@@ -74,12 +77,12 @@ df1 <- df1 %>% filter(!grepl(".", Name, fixed = TRUE))
   
 df1 <-distinct(df1, Name, LastName, .keep_all = TRUE)
   
-df2 <- data.frame(Position, Number, Birth, Altura, Pe_preferido, data_chegada, Valor)
+df2 <- data.frame(Position, Number, Birth, Height, Foot, Joined, Value)
   
 df <- cbind(df1,df2)
   
-df$season <- Clubs_full$Season[j]
-df$clube <- Clubs_full$clubs[j]
+df$Season <- TeamsFull$Season[j]
+df$Team <- TeamsFull$Teams[j]
 
 Players <- bind_rows(Players,df)
 }
@@ -91,7 +94,7 @@ Players <- bind_rows(Players,df)
 
 #Birth
 
-Players <- Players %>% separate(Birth, into = c("BirthDate", "age"), sep = "(?<=^\\S{0,1000})\\s+")
+Players <- Players %>% separate(Birth, into = c("BirthDate", "Age"), sep = "(?<=^\\S{0,1000})\\s+")
      
 Players$age <- gsub('(','', Players$age, fixed = TRUE)
 Players$age <- as.numeric(gsub(')','', Players$age, fixed = TRUE))
@@ -100,28 +103,113 @@ Players$age <- as.numeric(gsub(')','', Players$age, fixed = TRUE))
 
 Players$BirthDate <- as.Date(Players$BirthDate, '%d/%m/%Y')
 
-#Altura
+#Height
 
-Players$Altura <- gsub(' m','', Players$Altura, fixed = TRUE)
-Players$Altura <- as.numeric(gsub(',','.', Players$Altura, fixed = TRUE))
+Players$Height <- gsub(' m','', Players$Height, fixed = TRUE)
+Players$Height <- as.numeric(gsub(',','.', Players$Height, fixed = TRUE))
 
 #DataChegada
 
-Players$data_chegada <- as.Date(Players$data_chegada, '%d/%m/%Y')
+Players$Joined <- as.Date(Players$Joined, '%d/%m/%Y')
 
 
-#Valor
+#Value
 
-Players$Valor_num <- Players$Valor
+Players$Value_num <- Players$Value
 
 Players <- Players %>% 
-           mutate(Valor_num = gsub("€", "", Valor)) %>% 
-           mutate(Valor_num = gsub("M", "", Valor_num)) %>%
-           mutate(Valor_num = gsub(",", ".", Valor_num)) %>%
-           mutate(Valor_num = gsub("mil", "", Valor_num)) %>%
-           mutate(Valor_num = as.numeric(gsub("^\\s+|\\s+$", "", Valor_num))) %>%  
-           mutate(Valor_num = case_when(grepl("M", x$Valor) == TRUE ~ Valor_num,
-                              grepl("mil", x$Valor) == TRUE ~ Valor_num/1000,
+           mutate(Value_num = gsub("€", "", Value)) %>% 
+           mutate(Value_num = gsub("M", "", Value_num)) %>%
+           mutate(Value_num = gsub(",", ".", Value_num)) %>%
+           mutate(Value_num = gsub("mil", "", Value_num)) %>%
+           mutate(Value_num = as.numeric(gsub("^\\s+|\\s+$", "", Value_num))) %>%  
+           mutate(Value_num = case_when(grepl("M", Players$Value) == TRUE ~ Value_num,
+                              grepl("mil", Players$Value) == TRUE ~ Value_num/1000,
                               TRUE ~ 0))
 
 
+##################################################################################################################
+
+#####Pegando Tabela da Premiere League 2010 - 2018
+
+Standings <- NULL
+Leaders <- NULL
+
+for (j in PastSeasons) {
+  
+  url = paste("https://www.transfermarkt.com.br/premier-league/tabelle/wettbewerb/GB1/saison_id/", j, sep='')
+
+scraped_page <- read_html(url)
+
+Position  <- scraped_page %>% html_nodes(".responsive-table .rechts.hauptlink") %>% html_text() %>% as.character() %>% trimws("r", whitespace = "[\\h\\v]") %>% as.numeric()
+Team    <- scraped_page %>% html_nodes(".responsive-table .no-border-links") %>% html_text() %>% as.character() %>% 
+            trimws("r", whitespace = "[\\h\\v]") %>% trimws("l", whitespace = "[\\h\\v]")
+Games    <- scraped_page %>% html_nodes(".no-border-links+ .zentriert a") %>% html_text() %>% as.numeric()
+Win <- scraped_page %>% html_nodes(".zentriert:nth-child(5) a") %>% html_text() %>% as.numeric()
+Draw   <- scraped_page %>% html_nodes(".zentriert:nth-child(6) a") %>% html_text() %>% as.numeric()
+Lose <- scraped_page %>% html_nodes(".zentriert:nth-child(7) a") %>% html_text() %>% as.numeric()
+Goals     <- scraped_page %>% html_nodes("td:nth-child(8)") %>% html_text() %>% as.character()
+GoalsDif    <- scraped_page %>% html_nodes("td:nth-child(9)") %>% html_text() %>% as.numeric()
+Points   <- scraped_page %>% html_nodes(".zentriert:nth-child(10)") %>% html_text() %>% as.numeric()
+
+
+TeamLeader <- scraped_page %>% html_nodes(".large-8 .table-header+ table .hauptlink") %>% html_text() %>% as.character() %>% 
+              trimws("r", whitespace = "[\\h\\v]") %>% trimws("l", whitespace = "[\\h\\v]")
+RoundsLeader <- scraped_page %>% html_nodes(".large-8 .table-header+ table .hauptlink+ .zentriert") %>% html_text() %>% as.numeric()
+FistLeader  <- scraped_page %>% html_nodes(".zentriert+ td:nth-child(4)") %>% html_text() %>% as.character()
+LastLider  <- scraped_page %>% html_nodes(".large-8 .table-header+ table .zentriert:nth-child(5)") %>% html_text() %>% as.character()
+LastLider  <- c(LastLider, vector(mode="character", length=length(FistLeader)-length(LastLider)))
+
+
+df1 <- data.frame(Position, Team, Games, Win, Draw, Lose, Goals, GoalsDif, Points, stringsAsFactors = FALSE)
+df1 <- df1 %>% separate(Goals, into = c("GolsPro", "GolsContra"), sep = ":")
+df1$Season <- j 
+
+Standings <- bind_rows(Standings, df1)
+
+
+df2 <- data.frame(TeamLeader, RoundsLeader, FistLeader, LastLider, stringsAsFactors = FALSE)
+df2$Season <- j
+
+Leaders <- bind_rows(Leaders, df2)
+}
+
+############################################################################################
+
+
+########################
+#Jogos Premiere League
+#######################
+
+Matches <- NULL
+
+for (j in seq(1,nrow(TeamsFull),1)) {
+
+  url = paste("https://www.transfermarkt.co.uk/", TeamsFull$Teams[j], "/spielplan/verein/", TeamsFull$Cod[j], "/saison_id/", TeamsFull$Season[j], "/plus/1#GB1", sep='')
+  
+
+scraped_page <- read_html(url)
+
+MatchDay  <- scraped_page %>% html_nodes("td.zentriert:nth-child(1)") %>% html_text() %>% as.character() %>% trimws("r", whitespace = "[\\h\\v]")%>% trimws("l", whitespace = "[\\h\\v]") 
+Date      <- scraped_page %>% html_nodes(".responsive-table td:nth-child(2)") %>% html_text() %>% as.character()
+Time      <- scraped_page %>% html_nodes(".responsive-table td:nth-child(3)") %>% html_text() %>% as.character()
+HomeTeam  <- scraped_page %>% html_nodes(".no-border-links:nth-child(5)") %>% html_text() %>% as.character()
+AwayTeam  <- scraped_page %>% html_nodes(".no-border-links:nth-child(7)") %>% html_text() %>% as.character()
+
+Formation  <- scraped_page %>% html_nodes(".zentriert:nth-child(8)") %>% html_text() %>% as.character()
+Manager    <- scraped_page %>% html_nodes("td:nth-child(9)") %>% html_text() %>% as.character()
+Attendence <- scraped_page %>% html_nodes(".responsive-table td.rechts") %>% html_text() %>% as.numeric()
+Result     <- scraped_page %>% html_nodes(".rechts+ td") %>% html_text() %>% as.character()
+
+
+
+df1 <- data.frame(MatchDay, Date, Time, HomeTeam, AwayTeam, Formation, Manager, Attendence, Result, stringsAsFactors = FALSE)
+
+df1$TeamMatches <- TeamsFull$Teams[j]
+df1$Season <- TeamsFull$Season[j]
+
+Matches <- bind_rows(Matches, df1)
+}
+
+PremiereMatches <- Matches %>% filter(MatchDay %in% as.character(seq(1,38,1)))
+table(Matches$MatchDay)
